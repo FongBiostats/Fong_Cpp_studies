b1259218f2efa75fb9bb846ab3e285d75bcc35d1
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-10-31 03:04:51 +0800

    test

---
+++ atest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=10;
+    int y=8;
+    int z=16;
+    
+    cout << x+y+z << endl;
+    cout << x*z/y << endl;
+    cout << --x << endl; 
+    return 0;
+}
