8e8d2cddcf7c5100734c5ba434c24f25ee3d505b
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-10-31 11:30:46 +0800

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -3,28 +3,31 @@ using namespace std;
 
 int main()
 {
-    int country=3;
+    int country=10;
     switch(country)
     {
         case 1:
-        cout << "USA";
+        cout << "USA" <<endl;
         break;
 
         case 2:
-        cout << "RUSSIA";
+        cout << "RUSSIA" <<endl;
         break;
 
         case 3:
-        cout << "France";
+        cout << "France" <<endl;
         break;
 
         case 4:
-        cout << "JAPAN";
+        cout << "JAPAN" <<endl;
         break;
 
         case 5:
-        cout << "GERMANY";
+        cout << "GERMANY" <<endl;
         break;
+
+        default:
+        cout << "CHINA" << endl;
     }
     return 0;
 }
