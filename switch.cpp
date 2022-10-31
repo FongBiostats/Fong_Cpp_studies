b77cabaedb6fa68906ad00e1d4bb0e6bb66f93f9
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-10-31 11:53:16 +0800

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -29,5 +29,36 @@ int main()
         default:
         cout << "CHINA" << endl;
     }
+
+    int food=2;
+    switch (food)
+    {
+        case 1:
+        cout << "cake" <<endl;
+        break;
+
+        case 2:
+        cout << "egg" <<endl;
+        break;
+
+        case 3:
+        cout << "milk" <<endl;
+        break;
+
+        case 4:
+        cout << "sugar" <<endl;
+        break;
+
+        case 5:
+        cout << "pork" <<endl;
+        break;
+
+        case 6:
+        cout << "rice" <<endl;
+        break;
+
+        default:
+        cout << "apple" <<endl;
+    }
     return 0;
 }
