bbe64984bd55d15bf1a04f04dec56f0809b1b158
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-10-31 11:25:39 +0800

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int country=3;
+    switch(country)
+    {
+        case 1:
+        cout << "USA";
+        break;
+
+        case 2:
+        cout << "RUSSIA";
+        break;
+
+        case 3:
+        cout << "France";
+        break;
+
+        case 4:
+        cout << "JAPAN";
+        break;
+
+        case 5:
+        cout << "GERMANY";
+        break;
+    }
+    return 0;
+}
