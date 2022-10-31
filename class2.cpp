f642c1ba63c88af9258d619a12ba0fe9bb0b1207
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-10-31 18:48:49 +0800

    Add files via upload

--- class2.cpp
+++ class2.cpp
@@ -3,27 +3,37 @@ using namespace std;
 
 class Car{
     public:
-    string goods;
-    string tyle;
-    int price;
-    int date;
-    Car(string x, string y, int z, int w);
+    string name;
+    string nation;
+    string gender;
+    int age;
+    int height;
+    int weight;
+
+    Car(string x, string y, string z, int a, int b, int c);
 
 };
 
-Car::Car(string x, string y, int z, int w){
-    goods= x;
-    tyle= y;
-    price= z;
-    date= w;
+Car::Car( string x, string y, string z, int a, int b,int c){
+     name= x;
+     nation= y;
+     gender= z;
+     age= a; 
+     height= b;
+     weight= c;
 }
 
 int main(){
-    Car carobj1("fans","electrics",200,1996);
-    Car carobj2("television","electrics",1000,2001);
 
-    cout << carobj1.goods << " " << carobj1.tyle << " " << carobj1.price << " " << carobj1.date << "\n";
-    cout << carobj2.goods << " " << carobj2.tyle << " " << carobj2.price << " " << carobj2.date << "\n";
+     Car carobj1 ("Tom","Russia","male",20,178,60);
+     Car carobj2 ( "Ken","Germamy","male",32,185,75);
+     Car carobj3 ("Alice","France","Female",16,155,45);
+     Car carobj4 ("Sam","Japan","male",27,160,52);
+
+     cout << carobj1.name << " " << carobj1.nation << " " << carobj1.gender << " " << carobj1.age << " " << carobj1.height << " " << carobj1.weight << "\n";
+     cout << carobj2.name << " " << carobj2.nation << " " << carobj2.gender << " " << carobj2.age << " " << carobj2.height << " " << carobj2.weight << "\n";
+     cout << carobj3.name << " " << carobj3.nation << " " << carobj3.gender << " " << carobj3.age << " " << carobj3.height << " " << carobj4.weight << "\n";
+     cout << carobj4.name << " " << carobj4.nation << " " << carobj4.gender << " " << carobj4.age << " " << carobj4.height << " " << carobj4.weight << "\n";
 
     return 0;
 }
