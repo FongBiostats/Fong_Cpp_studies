94fe157ab0b21281d637c155d189cce2ddf39725
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-11-01 15:43:37 +0800

    Add files via upload

---
+++ 01_intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<< "hello world!" << endl;
+    cout<< "my name is FongBiostats" << endl;
+    cout<< "Welcome to my file, this file is about my note of C++";
+
+    return 0;
+}
