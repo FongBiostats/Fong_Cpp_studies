8443dce36e97888e798860eefcb1060d9c3df2ff
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-11-02 01:40:25 +0800

    Add files via upload

---
+++ 02.test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int people= 10.5; 
+    cout << people << endl;
+
+    float house =5.33;
+    cout << house << endl;
+
+    double tree = 6.4;
+    cout << tree << endl;
+
+    return 0;
+
+}
