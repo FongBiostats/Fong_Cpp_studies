c28ec5b0e92ca99e33c1a60abcab5c60c662495e
方野人Biostats <104591906+FongBiostats@users.noreply.github.com>
2022-11-04 17:39:53 +0800

    Add files via upload

---
+++ 01_game_note.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Game begin!" << endl;
+    cout << "Game over!" << endl;
+
+    return 0;
+
+}
---
+++ 02_game_note.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout << "18+3=" << 18+3 << endl;
+    cout << "18-3=" << 18-3 << endl;
+    cout << "18*3=" << 18*3 << endl;
+    cout << "18/3=" << 18/3 << endl;
+    cout << "5.0/2.5=" << 5.0/2.5 << endl;
+    cout << "9%3=" << 9%3 << endl;
+    cout << "10+10*2=" << 10+10*2 << endl;
+    cout << "(10+10)*2=" << (10+10)*2 << endl;
+
+    return 0;
+
+}
